d4b6b81068be77ed3fbebb724a161c72ad7ed51d
chen011209 <cyc7397221>
2023-11-23 16:01:54 +0800

    topic模式

--- mq-demo/consumer/src/main/java/cn/itcast/mq/listener/SpringRabbitListener.java
+++ mq-demo/consumer/src/main/java/cn/itcast/mq/listener/SpringRabbitListener.java
@@ -58,24 +58,25 @@ public class SpringRabbitListener {
     public void listenDirectQueue2(String msg){
         System.out.println("消费者接收到direct.queue2的消息：【" + msg + "】");
     }
-//
-//    @RabbitListener(bindings = @QueueBinding(
-//            value = @Queue(name = "topic.queue1"),
-//            exchange = @Exchange(name = "itcast.topic", type = ExchangeTypes.TOPIC),
-//            key = "china.#"
-//    ))
-//    public void listenTopicQueue1(String msg){
-//        System.out.println("消费者接收到topic.queue1的消息：【" + msg + "】");
-//    }
-//
-//    @RabbitListener(bindings = @QueueBinding(
-//            value = @Queue(name = "topic.queue2"),
-//            exchange = @Exchange(name = "itcast.topic", type = ExchangeTypes.TOPIC),
-//            key = "#.news"
-//    ))
-//    public void listenTopicQueue2(String msg){
-//        System.out.println("消费者接收到topic.queue2的消息：【" + msg + "】");
-//    }
+
+    //#代表匹配一个单词
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(name = "topic.queue1"),
+            exchange = @Exchange(name = "itcast.topic", type = ExchangeTypes.TOPIC),
+            key = "china.#"
+    ))
+    public void listenTopicQueue1(String msg){
+        System.out.println("消费者接收到topic.queue1的消息：【" + msg + "】");
+    }
+
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(name = "topic.queue2"),
+            exchange = @Exchange(name = "itcast.topic", type = ExchangeTypes.TOPIC),
+            key = "#.news"
+    ))
+    public void listenTopicQueue2(String msg){
+        System.out.println("消费者接收到topic.queue2的消息：【" + msg + "】");
+    }
 //
 //    @RabbitListener(queues = "object.queue")
 //    public void listenObjectQueue(Map<String,Object> msg){
--- mq-demo/publisher/src/test/java/cn/itcast/mq/spring/SpringAmqpTest.java
+++ mq-demo/publisher/src/test/java/cn/itcast/mq/spring/SpringAmqpTest.java
@@ -54,14 +54,14 @@ public class SpringAmqpTest {
         // 发送消息
         rabbitTemplate.convertAndSend(exchangeName, "red", message);
     }
-//
-//    @Test
-//    public void testSendTopicExchange() {
-//        // 交换机名称
-//        String exchangeName = "itcast.topic";
-//        // 消息
-//        String message = "今天天气不错，我的心情好极了!";
-//        // 发送消息
-//        rabbitTemplate.convertAndSend(exchangeName, "china.weather", message);
-//    }
+
+    @Test
+    public void testSendTopicExchange() {
+        // 交换机名称
+        String exchangeName = "itcast.topic";
+        // 消息
+        String message = "今天天气不错，我的心情好极了!";
+        // 发送消息
+        rabbitTemplate.convertAndSend(exchangeName, "china.weather", message);
+    }
 }
